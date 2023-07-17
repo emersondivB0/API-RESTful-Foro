71f6a0ec21ab5ddfc97cfa890f6910785c020e0c
Emerson Gonzalez <emerson.dvlmt@gmail.com>
2023-07-16 21:25:24 -0400

    Create Respuesta class for Responses tot he Topics

--- src/main/java/com/alura/foro/modelo/respuesta/Respuesta.java
+++ src/main/java/com/alura/foro/modelo/respuesta/Respuesta.java
@@ -2,10 +2,23 @@ package com.alura.foro.modelo.respuesta;
 
 import com.alura.foro.modelo.topico.Topico;
 import com.alura.foro.modelo.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity(name = "Respuesta")
+@Table(name = "respuestas")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+@Data
 public class Respuesta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String mensaje;
     private Topico topico;
@@ -13,76 +26,5 @@ public class Respuesta {
     private Usuario autor;
     private Boolean solucion = false;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Respuesta other = (Respuesta) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMensaje() {
-        return mensaje;
-    }
-
-    public void setMensaje(String mensaje) {
-        this.mensaje = mensaje;
-    }
-
-    public Topico getTopico() {
-        return topico;
-    }
-
-    public void setTopico(Topico topico) {
-        this.topico = topico;
-    }
-
-    public LocalDateTime getfechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setfechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public Usuario getAutor() {
-        return autor;
-    }
-
-    public void setAutor(Usuario autor) {
-        this.autor = autor;
-    }
-
-    public Boolean getSolucion() {
-        return solucion;
-    }
-
-    public void setSolucion(Boolean solucion) {
-        this.solucion = solucion;
-    }
 }
